8818bcde1bf47ea86b9f03dd50b5e19955247424
NEMESISORION <moryriyad3@gmail.com>
2026-09-21 20:42:28 +0300

    fix: remove redundant participant assertion

--- app/Http/Controllers/LiveSessionAnswerController.php
+++ app/Http/Controllers/LiveSessionAnswerController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Domain\LiveSessions\RecordLiveAnswer;
 use App\Http\Requests\StoreLiveAnswerRequest;
 use App\Models\LiveSession;
-use App\Models\LiveSessionParticipant;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 
@@ -19,7 +18,6 @@ class LiveSessionAnswerController extends Controller
         $learner = $request->user();
         assert($learner instanceof User);
         $participant = $liveSession->participants()->whereBelongsTo($learner, 'learner')->firstOrFail();
-        assert($participant instanceof LiveSessionParticipant);
 
         $recordLiveAnswer->handle($liveSession, $participant, $request->integer('answer_option_id'));
 
