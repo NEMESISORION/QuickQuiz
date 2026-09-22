0b5351ba9b14093b9e5107a163ae1f284ea0dfd8
NEMESISORION <moryriyad3@gmail.com>
2026-09-22 21:50:02 +0300

    style: order user model imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use App\Enums\UserRole;
 use App\Enums\ThemePreference;
+use App\Enums\UserRole;
 use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
